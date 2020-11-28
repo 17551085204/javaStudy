f5caf2f90182ba3b71449f5da86723dff0d30cda
hdk <2890241339@qq.com>
2020-11-28 23:27:59 +0800

    1
    1

--- javaBasic/src/itheima/day04_IDEA_方法/demo01/HelloWorld.java
+++ javaBasic/src/itheima/day04_IDEA_方法/demo01/HelloWorld.java
@@ -1,10 +1,11 @@
 package itheima.day04_IDEA_方法.demo01;
 
 public class HelloWorld {
-
     public static void main(String[] args) {
-
         System.out.println("Hello, World!111");
-    }
+        System.out.println("Hello, World!222");
+        System.out.println("Hello, World!333");
 
+
+    }
 }
