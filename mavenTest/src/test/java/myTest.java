ca987573fc0f0a78be5a6465601ecc09dc3bff0e
hdk <2890241339@qq.com>
2020-11-29 21:21:23 +0800

    1
    1

---
+++ mavenTest/src/test/java/myTest.java
@@ -0,0 +1,20 @@
+/*
+@Author:南柯一梦
+@Contact:2890241339@qq.com
+@Date:2020/11/29
+*/
+
+//import java.util.Scanner;
+
+import org.junit.Test;
+
+public class myTest {
+
+    @Test
+    public void addTest(){
+        System.out.println(1+2);
+    }
+
+
+
+}
