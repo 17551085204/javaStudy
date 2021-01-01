b793302e79f6395271dbe47f1e60c8cd2a4e9b31
hdk <2890241339@qq.com>
2021-01-01 20:49:02 +0800

    1
    1

---
+++ javaBasic/src/尚硅谷/java基础/day01_day05/day01/HelloWorld.java
@@ -0,0 +1,18 @@
+/*
+@Author:南柯一梦
+@Contact:2890241339@qq.com
+@Date:2021/1/1
+*/
+
+package 尚硅谷.java基础.day01_day05.day01;
+
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("hello world");
+        System.out.println("你好");
+
+    }
+
+}
