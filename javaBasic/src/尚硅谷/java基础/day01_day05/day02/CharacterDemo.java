c59e56dd50ef484911482ea81abbd09d5daa2d6d
hdk <2890241339@qq.com>
2021-01-03 16:56:48 +0800

    1
    1

--- javaBasic/src/尚硅谷/java基础/day01_day05/day02/CharacterDemo.java
+++ javaBasic/src/尚硅谷/java基础/day01_day05/day02/CharacterDemo.java
@@ -2,6 +2,6 @@ package 尚硅谷.java基础.day01_day05.day02;
 
 class CharacterDemo {
 	public static void main(String[] args) {
-		System.out.println("Hello World!��ã��й���");
+		System.out.println("Hello World!");
 	}
 }
--- javaBasic/src/尚硅谷/java基础/day01_day05/day02/VariableTest4.java
+++ javaBasic/src/尚硅谷/java基础/day01_day05/day02/VariableTest4.java
@@ -1,27 +1,27 @@
 package 尚硅谷.java基础.day01_day05.day02;
-
 class VariableTest4 {
 	public static void main(String[] args) {
-		
-		//1.�������1��
+
+		//1.编码情况1：
 		long l = 123213;
 		System.out.println(l);
-		//����ʧ�ܣ����������
+		//编译失败：过大的整数
 		//long l1 = 21332423235234123;
 		long l1 = 21332423235234123L;
 
 
 		//****************
-		//����ʧ��
+		//编译失败
 		//float f1 = 12.3;
 		float f1 = (float)12.3;
-		//2.�������2��
-		//���ͳ�����Ĭ������Ϊint��
-		//�����ͳ�����Ĭ������Ϊdouble��
+		//2.编码情况2：
+		//整型常量，默认类型为int型
+		//浮点型常量，默认类型为double型
 		byte b = 12;
-		//byte b1 = b + 1;//����ʧ��
+		//byte b1 = b + 1;//编译失败
 
-		//float f1 = b + 12.3;//����ʧ��
-		
+		//float f1 = b + 12.3;//编译失败
+
 	}
 }
+
