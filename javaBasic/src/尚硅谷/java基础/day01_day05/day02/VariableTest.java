3306c6ad6ade9ad363102ccaedea69939ec9905e
hdk <2890241339@qq.com>
2021-01-03 16:39:07 +0800

    1
    1

--- javaBasic/src/尚硅谷/java基础/day01_day05/day02/VariableTest.java
+++ javaBasic/src/尚硅谷/java基础/day01_day05/day02/VariableTest.java
@@ -1,39 +1,39 @@
 package 尚硅谷.java基础.day01_day05.day02;
 
 /*
-������ʹ��
-1. java��������ĸ�ʽ���������� ������ = ����ֵ;
+变量的使用
+1. java定义变量的格式：数据类型 变量名 = 变量值;
 
-2. ˵����
-   �� ������������������ʹ��
-   �� ���������������������ڡ����������ڣ�������Ч�ġ����仰˵�����������򣬾�ʧЧ��
-   �� ͬһ���������ڣ���������������ͬ���ı���
+2. 说明：
+   ① 变量必须先声明，后使用
+   ② 变量都定义在其作用域内。在作用域内，它是有效的。换句话说，出了作用域，就失效了
+   ③ 同一个作用域内，不可以声明两个同名的变量
 
 */
 class VariableTest {
 	public static void main(String[] args) {
-		//�����Ķ���
+		//变量的定义
 		int myAge = 12;
-		//������ʹ��
+		//变量的使用
 		System.out.println(myAge);
-		
-		//�������ʹ��myNumber֮ǰ��δ�����myNumber
+
+		//编译错误：使用myNumber之前并未定义过myNumber
 		//System.out.println(myNumber);
 
-		//����������
+		//变量的声明
 		int myNumber;
-		
-		//�������ʹ��myNumber֮ǰ��δ��ֵ��myNumber
+
+		//编译错误：使用myNumber之前并未赋值过myNumber
 		//System.out.println(myNumber);
 
-		//�����ĸ�ֵ
+		//变量的赋值
 		myNumber = 1001;
-		//���벻ͨ��
+		//编译不通过
 		//System.out.println(myClass);
 
-		//��������ͬһ���������ڶ���ͬ���ı���
+		//不可以在同一个作用域内定义同名的变量
 		//int myAge = 22;
-		
+
 	}
 
 	public void method(){
@@ -41,4 +41,4 @@ class VariableTest {
 	}
 }
 
-//class VariableTest {}//����˼ά����֤��
+//class VariableTest {}//逆向思维，反证法
