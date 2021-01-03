78b69d69887af800d8cd642ec75b5dff0c8ed0f9
hdk <2890241339@qq.com>
2021-01-03 19:48:59 +0800

    1
    1

--- javaBasic/src/尚硅谷/java基础/day01_day05/day02/StringTest.java
+++ javaBasic/src/尚硅谷/java基础/day01_day05/day02/StringTest.java
@@ -1,16 +1,15 @@
 package 尚硅谷.java基础.day01_day05.day02;
-
 /*
-String���ͱ�����ʹ��
-1. String����������������,����Ϊ���ַ���
-2. ����String���ͱ���ʱ��ʹ��һ��""
-3. String���Ժ�8�ֻ����������ͱ��������㣬������ֻ�����������㣺+
-4. ����Ľ����Ȼ��String����
+String类型变量的使用
+1. String属于引用数据类型,翻译为：字符串
+2. 声明String类型变量时，使用一对""
+3. String可以和8种基本数据类型变量做运算，且运算只能是连接运算：+
+4. 运算的结果仍然是String类型
 
 */
 class StringTest {
 	public static void main(String[] args) {
-		
+
 		String s1 = "Hello World!";
 
 		System.out.println(s1);
@@ -18,18 +17,18 @@ class StringTest {
 		String s2 = "a";
 		String s3 = "";
 
-		//char c = '';//���벻ͨ��
+		//char c = '';//编译不通过
 
 		//***********************
 		int number = 1001;
-		String numberStr = "ѧ�ţ�";
-		String info = numberStr + number;// +����������
+		String numberStr = "学号：";
+		String info = numberStr + number;// +：连接运算
 		boolean b1 = true;
-		String info1 = info + b1;// +����������
+		String info1 = info + b1;// +：连接运算
 		System.out.println(info1);
 
 		//***********************
-		//��ϰ1
+		//练习1
 		char c = 'a';//97   A:65
 		int num = 10;
 		String str = "hello";
@@ -39,7 +38,7 @@ class StringTest {
 		System.out.println((c + num) + str);//107hello
 		System.out.println(str + num + c);//hello10a
 
-		//��ϰ2
+		//练习2
 		//*	*
 		System.out.println("*	*");
 		System.out.println('*' + '\t' + '*');
@@ -50,10 +49,10 @@ class StringTest {
 
 		//***********************
 
-		//String str1 = 123;//���벻ͨ��
+		//String str1 = 123;//编译不通过
 		String str1 = 123 + "";
 		System.out.println(str1);//"123"
-		
+
 		//int num1 = str1;
 		//int num1 = (int)str1;//"123"
 
@@ -61,3 +60,4 @@ class StringTest {
 		System.out.println(num1);//123
 	}
 }
+
